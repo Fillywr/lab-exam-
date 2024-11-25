fb7bd62a399218fd2058024d589789dcf836c940
Fillywr <urwsnab@gmail.com>
2024-11-25 17:57:33 +0300

    waaaa

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+ public class Program {
+//  string wasan
+//  int 23
+//  double 7
+       
+       void main(){
+              // Console.ReadLine(string,int,double);
+              Console.WriteLine("my name is ");
+              Console.WriteLine("I am years old");
+              Console.WriteLine("my favorite number is");
+
+       }      
+  
+}
+
+// public class Book {
+//        void main(){
+
+//        }
+
+
+
+
+
+// }
+
+
+w
